289f170316bc852f2123aab034b339ad90b20cb7
Sidra Kousar <114879564+sidra2772@users.noreply.github.com>
2025-08-27 23:46:21 +0500

    Update database.py

--- server/coresite/settings/database.py
+++ server/coresite/settings/database.py
@@ -1,12 +1,18 @@
 from .environment import env
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': env('DATABASE_NAME'),
+#         'USER': env('DATABASE_USER'),
+#         'PASSWORD': env('PASSWORD'),
+#         'HOST': env('HOST'),
+#         'PORT': 5432,
+#         'ATOMIC_REQUESTS': True,
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('PASSWORD'),
-        'HOST': env('HOST'),
-        'PORT': 5432,
-        'ATOMIC_REQUESTS': True,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # Creates db.sqlite3 file in your project root
     }
 }
